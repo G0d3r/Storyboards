6107e85b2938ec344c71556633eea94e26b4fbe7
G0d3r <ernest.godlewski@icloud.com>
2019-11-10 19:45:25 +0100

    Made interactive app without writing any code

---
+++ Ratings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by ErnestG on 10/11/2019.
+//  Copyright © 2019 EG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
